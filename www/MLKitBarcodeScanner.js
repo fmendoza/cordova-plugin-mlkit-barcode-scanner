8062c04fb594ff72a8406b9fca12b75aedcd0639
Fernando <f.mendoza.galvez@gmail.com>
2022-01-15 13:06:21 -0600

    update getBarcodeFormatFlags routine

--- www/MLKitBarcodeScanner.js
+++ www/MLKitBarcodeScanner.js
@@ -3,9 +3,27 @@ var exec = require('cordova/exec');
 var getBarcodeFormatFlags = function (barcodeFormats) {
   var barcodeFormatFlag = 0;
 
+  var barcodeFormat = Object.freeze({
+    Code128: 1,
+    Code39: 2,
+    Code93: 4,
+    CodaBar: 8,
+    DataMatrix: 16,
+    EAN13: 32,
+    EAN8: 64,
+    ITF: 128,
+    QRCode: 256,
+    UPCA: 512,
+    UPCE: 1024,
+    PDF417: 2048,
+    Aztec: 4096,
+  });
+
   for (key in barcodeFormats) {
-    if (barcodeFormats.hasOwnProperty(key) && barcodeFormats[key]) {
-      barcodeFormatFlag += barcodeFormats[key];
+    if (barcodeFormat.hasOwnProperty(key) &&
+      barcodeFormats.hasOwnProperty(key) &&
+      barcodeFormats[key]) {
+      barcodeFormatFlag += barcodeFormat[key];
     }
   }
   return barcodeFormatFlag;

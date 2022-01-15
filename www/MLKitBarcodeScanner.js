58246b2554ddc2f23202e927d46e1e7d6536a45e
Fernando <f.mendoza.galvez@gmail.com>
2022-01-15 12:58:35 -0600

    add getBarcodeFormatFlags routine

--- www/MLKitBarcodeScanner.js
+++ www/MLKitBarcodeScanner.js
@@ -1,8 +1,21 @@
 var exec = require('cordova/exec');
 
+var getBarcodeFormatFlags = function (barcodeFormats) {
+  var barcodeFormatFlag = 0;
+
+  for (key in barcodeFormats) {
+    if (barcodeFormats.hasOwnProperty(key) && barcodeFormats[key]) {
+      barcodeFormatFlag += barcodeFormats[key];
+    }
+  }
+  return barcodeFormatFlag;
+};
+
 var MLKitBarcodeScanner = {
 
   scan: function (options, successCallback, errorCallback) {
+
+    options.barcodeFormats = getBarcodeFormatFlags(barcodeFormats);
 
     exec(successCallback, errorCallback,
       'cordova-plugin-mlkit-barcode-scanner',
